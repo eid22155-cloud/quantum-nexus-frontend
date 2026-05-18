af2fce65c0c06b7593cd2cbacaa8561cdbe11fa7
eid22155-cloud <eid22155@gmail.com>
2026-05-18 05:52:35 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './QuantumNexusDashboard.jsx' // هنا خلينا الـ App يستدعي ملف الكوانتم بتاعك مباشرة
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
